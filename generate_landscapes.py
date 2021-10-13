8965561a09d9b33565980c06ebc64ee33bcac678
Katherine Benjamin <katherine.benjamin@maths.ox.ac.uk>
2021-10-13 23:11:40 +0100

    generalise

--- generate_landscapes.py
+++ generate_landscapes.py
@@ -1,18 +1,22 @@
 import os
+import sys
 import numpy as np
 import landscapes as pl
 
-diagram_directory = './data/diagrams/interpolated_x6/'
-landscape_directory = './data/landscapes/interpolated_x6/'
+def main():
+    diagram_directory = sys.argv[1]
+    landscape_directory = sys.argv[2]
+    for filename in os.listdir(diagram_directory):
+        print(filename)
+        root, _ = filename.split('.')
+        ls_filename = landscape_directory + root + '.lan'
+        if not os.path.isfile(ls_filename):
+            print("Computing...")
+            barcode = np.loadtxt(diagram_directory + filename)
+            landscape = pl.generate_landscape(barcode.tolist())
+            landscape.save(ls_filename)
+        else:
+            print("Done!")
 
-for filename in os.listdir(diagram_directory):
-    print(filename)
-    root, _ = filename.split('.')
-    ls_filename = landscape_directory + root + '.lan'
-    if not os.path.isfile(ls_filename):
-        print("Computing...")
-        barcode = np.loadtxt(diagram_directory + filename)
-        landscape = pl.generate_landscape(barcode.tolist())
-        landscape.save(ls_filename)
-    else:
-        print("Done!")
+if __name__ == "__main__":
+    main()
