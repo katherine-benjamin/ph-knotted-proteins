4f56a4a8389e16b2cf2b6f6a8cdb8aba967d5ef4
Katherine Benjamin <katherine.benjamin@maths.ox.ac.uk>
2022-01-03 13:14:14 +0000

    final fixes

--- generate_landscapes.py
+++ generate_landscapes.py
@@ -7,16 +7,19 @@ def main():
     diagram_directory = sys.argv[1]
     landscape_directory = sys.argv[2]
     for filename in os.listdir(diagram_directory):
-        print(filename)
-        root, _ = filename.split('.')
-        ls_filename = landscape_directory + root + '.lan'
-        if not os.path.isfile(ls_filename):
-            print("Computing...")
-            barcode = np.loadtxt(diagram_directory + filename)
-            landscape = pl.generate_landscape(barcode.tolist())
-            landscape.save(ls_filename)
-        else:
-            print("Done!")
+        try:
+            print(filename)
+            root, _ = filename.split('.')
+            ls_filename = landscape_directory + root + '.lan'
+            if not os.path.isfile(ls_filename):
+                print("Computing...")
+                barcode = np.loadtxt(diagram_directory + filename)
+                landscape = pl.generate_landscape(barcode.tolist())
+                landscape.save(ls_filename)
+            else:
+                print("Done!")
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
